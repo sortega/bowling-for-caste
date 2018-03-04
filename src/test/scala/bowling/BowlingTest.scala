b5c50641acd5129e30077d63a0ef2d6c8a2b1e1d
Sebastian Ortega <sebastian.ortega@letgo.com>
2018-03-04 18:45:51 +0100

    RED: one frame without spares nor strikes
    Step 2: some pins are hit down but nothing fancy like spares or strikes.

--- src/test/scala/bowling/BowlingTest.scala
+++ src/test/scala/bowling/BowlingTest.scala
@@ -7,4 +7,8 @@ class BowlingTest extends FlatSpec with Matchers {
   "A gutter game" should "award no points" in {
     Bowling.scoreForRow(List.fill(20)(0)) shouldBe 0
   }
+
+  "A game without spares nor strikes" should "score all pins hit down" in {
+    Bowling.scoreForRow(1 :: 2 :: List.fill(18)(0)) shouldBe 3
+  }
 }
