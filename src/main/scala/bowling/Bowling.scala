f1ce29a6b7ddde7c8f9af7eb5cfaba4c036e631a
Sebastian Ortega <sebastian.ortega@letgo.com>
2018-03-04 19:11:35 +0100

    GREEN: c'mon...
    We need triangulation again

--- src/main/scala/bowling/Bowling.scala
+++ src/main/scala/bowling/Bowling.scala
@@ -4,5 +4,6 @@ object Bowling {
   type Roll = Int
   type Score = Int
 
-  def scoreForRow(rolls: List[Roll]): Score = rolls.sum
+  def scoreForRow(rolls: List[Roll]): Score =
+    rolls.sum + (if (rolls.take(2).sum == 10) rolls(3) else 0)
 }
