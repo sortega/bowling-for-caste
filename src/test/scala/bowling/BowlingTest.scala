0059f6dafbb524cb19286fee60146f9b46883f0e
Sebastian Ortega <sebastian.ortega@letgo.com>
2018-03-04 18:34:18 +0100

    RED: gutter game
    The simplest row is the gutter game: the ball is thrown into either of
    the channels around your lane. These channels are called "gutters".
    
    In the red phase we write code to test the smallest possible increment
    of functionality possible. The gutter game is very, very simple.

--- src/test/scala/bowling/BowlingTest.scala
+++ src/test/scala/bowling/BowlingTest.scala
@@ -2,4 +2,9 @@ package bowling
 
 import org.scalatest.{FlatSpec, Matchers}
 
-class BowlingTest extends FlatSpec with Matchers {}
+class BowlingTest extends FlatSpec with Matchers {
+
+  "A gutter game" should "award no points" in {
+    Bowling.scoreForRow(List.fill(20)(0)) shouldBe 0
+  }
+}
