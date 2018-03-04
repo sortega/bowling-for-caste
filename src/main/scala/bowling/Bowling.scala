b4e447064640e92a6cb1de3f6ead6550bbefdc17
Sebastian Ortega <sebastian.ortega@letgo.com>
2018-03-04 19:26:30 +0100

    GREEN: we have strikes

--- src/main/scala/bowling/Bowling.scala
+++ src/main/scala/bowling/Bowling.scala
@@ -9,10 +9,10 @@ object Bowling {
   def scoreForRow(rolls: List[Roll]): Score = {
     @tailrec
     def go(rolls: List[Roll], score: Int = 0): Score = rolls match {
-      case first :: second :: rest if first + second == 10 =>
-        go(rest, score + 10 + rest.head)
-      case first :: second :: rest => go(rest, score + first + second)
-      case _                       => score
+      case 10 :: rest                                      => go(rest, score + 10 + rest.take(2).sum)
+      case first :: second :: rest if first + second == 10 => go(rest, score + 10 + rest.head)
+      case first :: second :: rest                         => go(rest, score + first + second)
+      case _                                               => score
     }
     go(rolls)
   }
