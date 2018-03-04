ce362e9d4a7081aba8a7cda155615331f43a09a3
Sebastian Ortega <sebastian.ortega@letgo.com>
2018-03-04 19:20:12 +0100

    Still RED: refactor to scale local optimum
    Going for the `sum` thrown us into a corner. We need to make decisions
    while the sequence of rolls is iterated. So the first step in the
    direction of getting green is doing a refactor.
    
    In this case we want to iterate (recursively but choose your own style)
    frame by frame through the list. Note that the previous tests are still
    green but the new one is still red.

--- src/main/scala/bowling/Bowling.scala
+++ src/main/scala/bowling/Bowling.scala
@@ -1,9 +1,17 @@
 package bowling
 
+import scala.annotation.tailrec
+
 object Bowling {
   type Roll = Int
   type Score = Int
 
-  def scoreForRow(rolls: List[Roll]): Score =
-    rolls.sum + (if (rolls.take(2).sum == 10) rolls(3) else 0)
+  def scoreForRow(rolls: List[Roll]): Score = {
+    @tailrec
+    def go(rolls: List[Roll], score: Int = 0): Score = rolls match {
+      case first :: second :: rest => go(rest, score + first + second)
+      case _                       => score
+    }
+    go(rolls)
+  }
 }
