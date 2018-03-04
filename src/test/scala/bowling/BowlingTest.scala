c1eec847ca71c9598b58681146432806afdce32e
Sebastian Ortega <sebastian.ortega@letgo.com>
2018-03-04 19:43:45 +0100

    YELLOW: go DSLish on the test
    We have a number of tests that are more legible if we can see the actual
    frames explicitly in the test.

--- src/test/scala/bowling/BowlingTest.scala
+++ src/test/scala/bowling/BowlingTest.scala
@@ -6,21 +6,29 @@ import Bowling._
 
 class BowlingTest extends FlatSpec with Matchers {
 
+  private def frame(rolls: Roll*): List[Roll] = rolls.toList
+
+  private val strike = frame(10)
+
+  private implicit class FrameOps(val frame: List[Roll]) {
+    def *(times: Int): List[Roll] = List.fill(times)(frame).flatten
+  }
+
   "A gutter game" should "award no points" in {
-    Bowling.scoreForRow(List.fill(2 * NumFrames)(0)) shouldBe 0
+    Bowling.scoreForRow(frame(0, 0) * NumFrames) shouldBe 0
   }
 
   "A game without spares nor strikes" should "score all pins hit down" in {
-    Bowling.scoreForRow(1 :: 2 :: List.fill(18)(0)) shouldBe 3
-    Bowling.scoreForRow(List.fill(NumFrames)(List(4, 2)).flatten) shouldBe 6 * 10
+    Bowling.scoreForRow(frame(1, 2) ++ frame(0, 0) * 9) shouldBe 3
+    Bowling.scoreForRow(frame(4, 2) * NumFrames) shouldBe 6 * 10
   }
 
   "A game without strikes" should "count twice the pins hit down after a spare" in {
-    Bowling.scoreForRow(5 :: 5 :: List.fill(18)(1)) shouldBe 10 + 18 + 1
-    Bowling.scoreForRow(2 :: 2 :: 9 :: 1 :: List.fill(16)(2)) shouldBe 10 + 18 * 2 + 2
+    Bowling.scoreForRow(frame(5, 5) ++ frame(1, 1) * 9) shouldBe 10 + 18 + 1
+    Bowling.scoreForRow(frame(2, 2) ++ frame(9, 1) ++ frame(2, 2) * 8) shouldBe 10 + 9 * 4 + 2
   }
 
   "A game" should "count twice the two rolls after a strike" in {
-    Bowling.scoreForRow(NumPins :: List.fill(18)(1)) shouldBe 10 + 18 + 2
+    Bowling.scoreForRow(strike ++ frame(1, 1) * 9) shouldBe 10 + 18 + 2
   }
 }
